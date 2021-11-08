768848c9de6240e69f7c9251910025260a94b053
Peterliang233 <ncuyanping666@126.com>
2021-11-08 21:31:10 +0800

    :zap: 修改跨域中件

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,20 +1,25 @@
 package middleware
 
 import (
-	"github.com/gin-contrib/cors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 // Cors 跨域中间件
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cors.New(cors.Config{
-			AllowOrigins:  []string{"*"},
-			AllowMethods:  []string{"*"},
-			AllowHeaders:  []string{"*"},
-			ExposeHeaders: []string{"Content-Length", "Authorization"},
-			MaxAge:        12 * time.Hour,
-		})
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length,Access-Control-Allow-Origin, Access-Control-Allow-Headers,Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		c.Next()
 	}
 }
